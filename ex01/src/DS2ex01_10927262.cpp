283c7d1e0375a85c42e4a6d4ea93c60a6b6104fd
YHL <123497542+tw89Coder@users.noreply.github.com>
2025-02-24 21:27:32 +0800

    Create ex01 file

---
+++ ex01/src/DS2ex01_10927262.cpp
@@ -0,0 +1,16 @@
+/** 
+ * @file DS2ex01_10927262.cpp
+ * @brief A program that uses Heap to manage graduate data.
+ * @version 0.0.0
+ *
+ * @details
+ * This program provides functionalities to read and process graduate data stored in files
+ * using a Heap as the underlying data structure. The main features include:
+ * - 123
+ *
+ * The program supports input files formatted in Tab-separated values (TSV) and organizes
+ * the data in the BST with graduate numbers as the key.
+ *
+ * @author 
+ * - 10927262 呂易鴻
+ */
